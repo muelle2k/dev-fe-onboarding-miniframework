88c809e53635bc6cec4cb64cfcbfc1755e1e3710
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-19 14:51:11 +0100

    now same scss file for jquery and js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 //jQuery change SCSS file first
 $('.contactUs').hide();
 $('.imageText__button').click(function (event) {
+  $(".contactUs").removeClass("contactUs--origin");
   $(event.target).closest('section').find('.contactUs').slideToggle(400);
 });
