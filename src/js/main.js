502cec8df26b39872adafe5080f5a949400cc2f4
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-19 12:59:17 +0100

    test

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,23 @@
 //ES5
 //PLAIN JS change SCSS file first
 
+
+
+
+
 function slide(elementToSlideOpen) {
+  console.log(elementToSlideOpen[0])
+  var element = elementToSlideOpen[0];
 
-  if (elementToSlideOpen.classList.contains('contactUs--origin', 'contactUs--hidden')) {
+  if (element.classList.contains('contactUs--origin', 'contactUs--hidden')) {
 
-    elementToSlideOpen.classList.remove('contactUs--origin', 'contactUs--hidden');
-    elementToSlideOpen.classList.add('contactUs--visible');
+    element.classList.remove('contactUs--origin', 'contactUs--hidden');
+    element.classList.add('contactUs--visible');
 
-  } else if (elementToSlideOpen.classList.contains('contactUs--visible')) {
+  } else if (element.classList.contains('contactUs--visible')) {
 
-    elementToSlideOpen.classList.remove('contactUs--visible');
-    elementToSlideOpen.classList.add('contactUs--hidden', 'contactUs--origin');
+    element.classList.remove('contactUs--visible');
+    element.classList.add('contactUs--hidden', 'contactUs--origin');
 
   }
 }
@@ -21,5 +27,5 @@ var button = document.getElementsByClassName('imageText__button');
 for (var i = 0; i < button.length; i++) {
   button[i].addEventListener('click', function e(event) {
     slide(event.target.closest('section').getElementsByClassName('contactUs'))
-  });
+  })
 }
