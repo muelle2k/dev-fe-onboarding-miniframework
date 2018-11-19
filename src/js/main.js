dc648fbe71d3268a4fae37653e3492db000077c4
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-19 11:03:23 +0100

    ES5 not yet finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,25 @@
-//jQuery change SCSS file first
-$('.contactUs').hide();
-$('.imageText__button').click(function (event) {
-  $(event.target).closest('section').find('.contactUs').slideToggle(400);
-});
+//ES5
+//PLAIN JS change SCSS file first
+
+function slide(elementToSlideOpen) {
+
+  if (elementToSlideOpen.classList.contains('contactUs--origin', 'contactUs--hidden')) {
+
+    elementToSlideOpen.classList.remove('contactUs--origin', 'contactUs--hidden');
+    elementToSlideOpen.classList.add('contactUs--visible');
+
+  } else if (elementToSlideOpen.classList.contains('contactUs--visible')) {
+
+    elementToSlideOpen.classList.remove('contactUs--visible');
+    elementToSlideOpen.classList.add('contactUs--hidden', 'contactUs--origin');
+
+  }
+}
+
+var button = document.getElementsByClassName('imageText__button');
+
+for (var i = 0; i < button.length; i++) {
+  button[i].addEventListener('click', function e(event) {
+    slide(event.target.closest('section').getElementsByClassName('contactUs'))
+  });
+}
