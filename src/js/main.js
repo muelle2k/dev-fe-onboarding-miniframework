496adb00d03e545488d2ce2ed846f0ace17c5994
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-19 10:40:05 +0100

    ES6 js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,29 @@
-//PLAIN JS SCSS Datei umändern
+//ES6
+//PLAIN JS change SCSS file first
 const namespace = {
 
   slide(elementToSlideOpen) {
 
-    if (elementToSlideOpen.classList.contains("contactUs--origin", "contactUs--hidden")) {
+    if (elementToSlideOpen.classList.contains('contactUs--origin', 'contactUs--hidden')) {
 
-      elementToSlideOpen.classList.remove("contactUs--origin", "contactUs--hidden");
-      elementToSlideOpen.classList.add("contactUs--visible");
+      elementToSlideOpen.classList.remove('contactUs--origin', 'contactUs--hidden');
+      elementToSlideOpen.classList.add('contactUs--visible');
 
-    } else if (elementToSlideOpen.classList.contains("contactUs--visible")) {
+    } else if (elementToSlideOpen.classList.contains('contactUs--visible')) {
 
-      elementToSlideOpen.classList.remove("contactUs--visible");
-      elementToSlideOpen.classList.add("contactUs--hidden", "contactUs--origin");
+      elementToSlideOpen.classList.remove('contactUs--visible');
+      elementToSlideOpen.classList.add('contactUs--hidden', 'contactUs--origin');
 
     }
   }
 }
 
-let button = document.getElementsByClassName("imageText__button");
+let button = document.querySelectorAll('.imageText__button');
 
 for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", (event) => {
+  button[i].addEventListener('click', (event) => {
     console.log(event.target);
 
     namespace.slide(event.target.closest('section').querySelector('.contactUs'))
   })
 }
-
-
-
-
-
-
-/*
-//jQuery SCSS Datei umändern
-
-$(".imageText__button").click(function () {
-  if ($(".contactUs").is(":hidden")) {
-    $(".contactUs").slideDown(400);
-  } else {
-    $(".contactUs").slideUp(400);
-  }
-})
-*/
