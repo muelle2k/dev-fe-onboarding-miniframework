6065be387f48c80dff5f5b84622223f5370ea49c
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-21 10:42:29 +0100

    removed the auskommentierte

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,3 @@
-//jQuery change SCSS file first
-// $('.contactUs').hide();
-// $('.imageText__button').click(function (event) {
-//   $(".contactUs").removeClass("contactUs--origin");
-//   $(event.target).closest('section').find('.contactUs').slideToggle(500);
-// });
-
 $('.contactUs').hide();
 $('.imageText__button').click(function (event) {
   $(".contactUs").removeClass("contactUs--origin");
