03aa7b6e2744f35325098d481b369d1a6f1d6e25
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-21 16:53:31 +0100

    errors in init()

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 //ES6
-const namespace = {
+const contactUs = {
 
   slideDown(elementToSlideOpen) {
     console.log(elementToSlideOpen)
@@ -11,12 +11,48 @@ const namespace = {
     console.log(element)
     element.classList.remove('contactUs--visible');
     element.classList.add('contactUs--hidden', 'contactUs--origin');
+  },
+  init() {
+    let button = document.querySelectorAll('.imageText__button');
+    for (let i = 0; i < button.length; i++) {
+      button[i].addEventListener('click', (event) => {
+        console.log(event.target);
+
+        document.querySelectorAll('.contactUs').forEach(element => {
+          if (event.target.closest('section').querySelector('.contactUs') !== element) {
+            slideUp(element)
+          }
+        });
+        slideDown(event.target.closest('section').querySelector('.contactUs'));
+      })
+    }
   }
 }
+console.log(contactUs.init());
+//ERROR: Uncaught ReferenceError: slideUp is not defined
+// at document.querySelectorAll.forEach.element (main.js:23)
+// at NodeList.forEach (<anonymous>)
+// at HTMLButtonElement.button.(:1337/anonymous function).addEventListener (http://localhost:1337/js/main.js:21:49)
 
-let button = document.querySelectorAll('.imageText__button');
+
+// for (let i = 0; i < contactUs.button.length; i++) {
+//   contactUs.button[i].addEventListener('click', (event) => {
+//     console.log(event.target);
+
+//     document.querySelectorAll('.contactUs').forEach(element => {
+//       if (event.target.closest('section').querySelector('.contactUs') !== element) {
+//         contactUs.slideUp(element)
+//       }
+//     });
+//     contactUs.slideDown(event.target.closest('section').querySelector('.contactUs'));
+//   })
+
+// }
+
+
+/*
+trying something with costumEvent()
 let contactUs = document.querySelectorAll('.contactUs')
-
 for (let i = 0; i < button.length; i++) {
   button[i].addEventListener('click', (event) => {
     console.log(event.target);
@@ -38,8 +74,6 @@ for (let i = 0; i < contactUs.length; i++) {
   contactUs.dispatchEvent(event);
 }
 
-
-
 for (let i = 0; i < button.length; i++) {
   button[i].addEventListener('clickedButton', (e) => {
     console.log("button has been clicked")
@@ -53,22 +87,4 @@ for (let i = 0; i < button.length; i++) {
   button.dispatchEvent(event);
 }
 
-
-
-
-
-
-// let button = document.querySelectorAll('.imageText__button');
-
-// for (let i = 0; i < button.length; i++) {
-//   button[i].addEventListener('click', (event) => {
-//     console.log(event.target);
-
-//     document.querySelectorAll('.contactUs').forEach(element => {
-//       if (event.target.closest('section').querySelector('.contactUs') !== element) {
-//         namespace.slideUp(element)
-//       }
-//     });
-//     namespace.slideDown(event.target.closest('section').querySelector('.contactUs'));
-//   })
-// }
+*/
