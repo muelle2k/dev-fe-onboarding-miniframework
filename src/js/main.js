f7a0faf99fd8efedc926788556a047322a5dc90b
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-21 12:46:25 +0100

    removed the auskommentierte

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,3 @@
-//ES5
-//PLAIN JS change SCSS file first
-
-// function slideDown(elementToSlideOpen) {
-//   console.log(elementToSlideOpen[0])
-//   var element = elementToSlideOpen[0].classList;
-//   console.log(element)
-
-//   if (element.length == 3) {
-
-//     element.remove("contactUs--origin", "contactUs--hidden");
-//     element.add("contactUs--visible");
-//     console.log(element)
-
-//   } else if (element.length == 2) {
-
-//     element.remove("contactUs--visible");
-//     element.add("contactUs--hidden", "contactUs--origin");
-
-//   }
-// }
-
 function slideDown(elementToSlideOpen) {
   elementToSlideOpen[0].classList.remove("contactUs--origin", "contactUs--hidden");
   elementToSlideOpen[0].classList.add("contactUs--visible");
