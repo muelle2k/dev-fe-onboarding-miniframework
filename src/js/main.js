93775d38b1aa4777a6883a3c7812c072838e9fb6
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-21 10:41:28 +0100

    events challenge one call-to-action with jQuery done

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 //jQuery change SCSS file first
+// $('.contactUs').hide();
+// $('.imageText__button').click(function (event) {
+//   $(".contactUs").removeClass("contactUs--origin");
+//   $(event.target).closest('section').find('.contactUs').slideToggle(500);
+// });
+
 $('.contactUs').hide();
 $('.imageText__button').click(function (event) {
   $(".contactUs").removeClass("contactUs--origin");
-  $(event.target).closest('section').find('.contactUs').slideToggle(400);
+  $(event.target).closest('section').find('.contactUs').slideToggle(500);
+  $('.contactUs').not($(event.target).closest('section').find('.contactUs')).slideUp(500);
 });
