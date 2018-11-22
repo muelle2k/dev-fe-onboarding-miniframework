b35ccc15d395114d9ad258de281d9f49a92639fb
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-22 11:47:40 +0100

    everything in the namespace

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,40 @@
-function slideDown(elementToSlideOpen) {
-  elementToSlideOpen[0].classList.remove("contactUs--origin", "contactUs--hidden");
-  elementToSlideOpen[0].classList.add("contactUs--visible");
+console.log("ES5")
+var contactUs = {
+  button: document.getElementsByClassName("imageText__button"),
+  ctaSection: document.querySelectorAll('.contactUs'),
+  opened: "contactUs--visible",
+  closed: "contactUs--hidden",
+  orgin: "contactUs--origin",
+
+  slideDown: function (elementToSlideOpen) {
+    elementToSlideOpen[0].classList.remove(this.closed, this.orgin);
+    elementToSlideOpen[0].classList.add(this.opened);
+  },
+
+  slideUp: function (element) {
+    element.classList.remove(this.opened);
+    element.classList.add(this.closed);
+  },
+
+  init: function () {
+    var self = this;
+    var button = self.button;
+    var ctaSection = self.ctaSection;
+
+    for (var i = 0; i < button.length; i++) {
+      button[i].addEventListener("click", function e(event) {
+        ctaSection.forEach(function (element) {
+          var contactUsUnderClickedButton = event.target.closest('section').getElementsByClassName('.contactUs');
+
+          if (contactUsUnderClickedButton !== element) {
+
+            self.slideUp(element);
+
+          }
+        });
+        self.slideDown(event.target.closest("section").getElementsByClassName("contactUs"))
+      })
+    }
+  }
 }
-
-function slideUp(element) {
-  console.log(element.classList)
-  element.classList.remove("contactUs--visible");
-  element.classList.add("contactUs--hidden", "contactUs--origin");
-}
-
-var button = document.getElementsByClassName("imageText__button");
-var ctaSection = document.querySelectorAll('.contactUs');
-console.log(ctaSection)
-
-for (var i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", function e(event) {
-
-    ctaSection.forEach(function (element) {
-
-      if (event.target.closest('section').getElementsByClassName('.contactUs') !== element) {
-
-        slideUp(element)
-      }
-    });
-    slideDown(event.target.closest("section").getElementsByClassName("contactUs"))
-  })
-}
+contactUs.init()
