0a54529c330faae17ccd9a26057b10a84045e420
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-22 12:09:56 +0100

    everything in the namespace

--- src/js/main.js
+++ src/js/main.js
@@ -1,95 +1,41 @@
-//ES6
+console.log("ES6");
 const contactUs = {
 
-  // button: document.querySelectorAll('.imageText__button'),
-  // slideDown(elementToSlideOpen) {
-  //   console.log(elementToSlideOpen)
-  //   elementToSlideOpen.classList.remove('contactUs--origin', 'contactUs--hidden');
-  //   elementToSlideOpen.classList.add('contactUs--visible');
-  // },
+  button: document.getElementsByClassName("imageText__button"),
+  ctaSection: document.querySelectorAll('.contactUs'),
+  opened: "contactUs--visible",
+  closed: "contactUs--hidden",
+  origin: "contactUs--origin",
 
-  // slideUp(element) {
-  //   console.log(element)
-  //   element.classList.remove('contactUs--visible');
-  //   element.classList.add('contactUs--hidden', 'contactUs--origin');
-  // },
+  slideDown(elementToSlideOpen) {
+    elementToSlideOpen.classList.remove(this.origin, this.closed);
+    elementToSlideOpen.classList.add(this.opened);
+  },
+
+  slideUp(element) {
+    console.log(element)
+    element.classList.remove(this.opened);
+    element.classList.add(this.origin, this.closed);
+  },
 
   init() {
-    let button = document.querySelectorAll('.imageText__button');
+    let self = this;
+    let button = self.button;
+    let ctaSection = self.ctaSection;
+
     for (let i = 0; i < button.length; i++) {
       button[i].addEventListener('click', (event) => {
-        console.log(event.target);
 
-        document.querySelectorAll('.contactUs').forEach(element => {
-          if (event.target.closest('section').querySelector('.contactUs') !== element) {
-            // slideUp(element)
-            element.classList.remove('contactUs--visible');
-            element.classList.add('contactUs--hidden', 'contactUs--origin');
+        var contactUsUnderClickedButton = event.target.closest('section').querySelector('.contactUs');
+
+        ctaSection.forEach(element => {
+          if (contactUsUnderClickedButton !== element) {
+            self.slideUp(element);
           }
         });
-        // slideDown(event.target.closest('section').querySelector('.contactUs'));
-        event.target.closest('section').querySelector('.contactUs').classList.remove('contactUs--origin', 'contactUs--hidden');
-        event.target.closest('section').querySelector('.contactUs').classList.add('contactUs--visible');
+        self.slideDown(event.target.closest('section').querySelector('.contactUs'));
       })
     }
   }
 }
-
-//console.log(contactUs.init())
-contactUs.init()
-
-
-
-// for (let i = 0; i < contactUs.button.length; i++) {
-//   contactUs.button[i].addEventListener('click', (event) => {
-//     console.log(event.target);
-
-//     document.querySelectorAll('.contactUs').forEach(element => {
-//       if (event.target.closest('section').querySelector('.contactUs') !== element) {
-//         contactUs.slideUp(element)
-//       }
-//     });
-//     contactUs.slideDown(event.target.closest('section').querySelector('.contactUs'));
-//   })
-
-// }
-
-
-/*
-trying something with costumEvent()
-let contactUs = document.querySelectorAll('.contactUs')
-for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener('click', (event) => {
-    console.log(event.target);
-
-    namespace.slideDown(event.target.closest('section').querySelector('.contactUs'));
-  })
-}
-
-for (let i = 0; i < contactUs.length; i++) {
-  contactUs[i].addEventListener('clickedButton', (e) => {
-    console.log("button has been clicked")
-
-  })
-
-  let event = new CostumEvent('clickedButton', {
-
-  });
-
-  contactUs.dispatchEvent(event);
-}
-
-for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener('clickedButton', (e) => {
-    console.log("button has been clicked")
-
-  })
-
-  let event = new CostumEvent('clickedButton', {
-
-  });
-
-  button.dispatchEvent(event);
-}
-
-*/
+contactUs.init();
