2058cfce0d91c91c89a41c482aad88eb2270c5cc
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-26 13:21:56 +0100

    one call-to-action with jQuery customEvent

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 console.log('jQuery');
+/*
 $('.imageText__button ').click(function (event) {
+  $('.contactUs').removeClass('contactUs--closed');
+  $(event.target).closest('section').find('.contactUs').addClass('contactUs--opened');
+  $('.contactUs').not($(event.target).closest('section').find('.contactUs')).removeClass('contactUs--opened').addClass('contactUs--closed');
+});
+*/
+
+$('.imageText__button ').click(function (event) {
+  $('.contactUs').trigger('clickedButton', event);
+});
+
+$('.contactUs').on('clickedButton', function () {
   $('.contactUs').removeClass('contactUs--closed');
   $(event.target).closest('section').find('.contactUs').addClass('contactUs--opened');
   $('.contactUs').not($(event.target).closest('section').find('.contactUs')).removeClass('contactUs--opened').addClass('contactUs--closed');
