22e7952aed440aa28bce09111162982da94ab26e
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2018-11-26 09:46:49 +0100

    classnames change in html but scss doesnt react on it

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
-console.log("jQuery");
+console.log('jQuery');
 $('.contactUs').hide();
-$('.imageText__button').click(function (event) {
-  $(".contactUs").removeClass("contactUs--origin");
-  $(event.target).closest('section').find('.contactUs').slideToggle(500);
-  $('.contactUs').not($(event.target).closest('section').find('.contactUs')).slideUp(500);
+
+$('.imageText__button ').click(function (event) {
+  console.log(event.target);
+
+  $('.contactUs').removeClass('contactUs--closed');
+  console.log($('.contactUs ').removeClass('contactUs--closed'));
+
+  $(event.target).closest('section').find('.contactUs').addClass('contactUs--opened');
+  $('.contactUs').not($(event.target).closest('section').find('.contactUs')).removeClass('contactUs--opened').addClass('contactUs--closed');
 });
