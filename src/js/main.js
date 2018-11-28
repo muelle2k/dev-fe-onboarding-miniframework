774e6a1a9bf7d28432b361a18fabfc93dcf29cb3
Patrick Müller-Klug <patrick.mueller-klug@virtual-identity.com>
2018-11-28 13:29:40 +0100

    removed the commented code

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,4 @@
 console.log('jQuery');
-/*
-$('.imageText__button ').click(function (event) {
-  $('.contactUs').removeClass('contactUs--closed');
-  $(event.target).closest('section').find('.contactUs').addClass('contactUs--opened');
-  $('.contactUs').not($(event.target).closest('section').find('.contactUs')).removeClass('contactUs--opened').addClass('contactUs--closed');
-});
-*/
-
 $('.imageText__button ').click(function (event) {
   $('.contactUs').trigger('clickedButton', event);
 });
